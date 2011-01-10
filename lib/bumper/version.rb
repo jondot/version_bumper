729cd2132eb4eaad18ef23600bf6737e1832960f
Peter Suschlik <ps@neopoly.de>
2011-01-10 11:13:26 +0100

    Use symbol keys instead of string keys

--- lib/bumper/version.rb
+++ lib/bumper/version.rb
@@ -1,7 +1,7 @@
 module Bumper
   class Version
     
-    ['major', 'minor', 'revision', 'build'].each do |part|
+    [:major, :minor, :revision, :build].each do |part|
       define_method part do 
         @v[part]
       end
@@ -14,22 +14,22 @@ module Bumper
     def initialize(v)
       @v = {}
       if v =~ /^(\d+)\.(\d+)\.(\d+)(?:\.(.*?))?$/
-          @v['major'] = $1.to_i
-          @v['minor'] = $2.to_i
-          @v['revision'] = $3.to_i
-          @v['build'] = $4
+          @v[:major]    = $1.to_i
+          @v[:minor]    = $2.to_i
+          @v[:revision] = $3.to_i
+          @v[:build]    = $4
       end
     end
     
     def bump(part)
       @v[part] = @v[part].succ
       
-      return @v[part] if part == 'build'      
-      @v['build'] = '0'
-      return @v[part] if part == 'revision'
-      @v['revision'] = 0
-      return @v[part] if part == 'minor'
-      @v['minor'] = 0
+      return @v[part] if part == :build
+      @v[:build] = '0'
+      return @v[part] if part == :revision
+      @v[:revision] = 0
+      return @v[part] if part == :minor
+      @v[:minor] = 0
       @v[part]
     end
     
